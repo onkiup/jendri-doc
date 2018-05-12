2e8494d0bb344ab16498a0c7a45ce222b9c08a7b
Dmitrii Chechetkin <dchechetkin@politico.com>
2018-05-12 19:45:05 -0400

    -- destination bug

--- services/navigation.js
+++ services/navigation.js
@@ -5,15 +5,15 @@
 me.router = 'router';
 
 var navigationEventListener = function(event) {
+    var target = $(event.target);
+    var destination = target.attr('href') || target.attr('submit');
     if (destination.match("javascript:")){
         return;
     }
-    event.preventDefault();
-    var target = $(event.target);
-    var destination = target.attr('href') || target.attr('submit');
     if (destination.substr(0, 1) === '#') {
         destination = destination.substr(1);
     }
+    event.preventDefault();
     me.router.navigate(destination, function () {
         history.pushState({}, window.title, jj.baseURL() + destination);
     });
