ef0d3ece1f8f9140a596dd5f00d726ca87850b05
Dmitrii Chechetkin <dchechetkin@politico.com>
2018-05-12 21:06:56 -0400

    handling back navigation

--- services/navigation.js
+++ services/navigation.js
@@ -4,29 +4,36 @@
 
 me.router = 'router';
 
-var navigate = function (destination) {
+var navigate = function (destination, back) {
     me.router.navigate(destination, function (err) {
-        if (!err) {
+        if (!back) {
             history.pushState({}, window.title, jj.base + destination);
         }
     });
 }
-var navigationEventListener = function(event) {
+var navigationEventListener = function (event) {
     var target = $(event.target);
     var destination = target.attr('href') || target.attr('submit');
-    if (destination.match("javascript:")){
-        return;
-    }
-    if (destination.substr(0, 1) === '#') {
-        destination = destination.substr(1);
+    var back = false;
+    if (destination) {
+        if (destination.match("javascript:")) {
+            return;
+        }
+        if (destination.substr(0, 1) === '#') {
+            destination = destination.substr(1);
+        }
+    } else {
+        destination = location.pathname.replace(jj.base, "");
+        back = true;
     }
     event.preventDefault();
-    navigate(destination);
+    navigate(destination, back);
 }
 
-me.create = function() {
+me.create = function () {
     $(document).on('click', 'a', navigationEventListener);
-    $(document).on('submit', 'form', navigationEventListener);
+    // $(document).on('submit', 'form', navigationEventListener);
+    $(window).on('popstate', navigationEventListener);
     if (document.location.hash) {
         navigate(document.location.hash.substr(1));
     } else if (document.location.pathname !== jj.base) {
